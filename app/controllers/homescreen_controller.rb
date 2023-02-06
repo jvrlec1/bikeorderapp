73ba1f58bf5e7cb9f2e32ddd17e596bf0590eb5f
jvrlec1 <jvrlec1@Ubuntu22.04.myguest.virtualbox.org>
2023-02-06 18:56:00 +0100

    Orders nil value fix

--- app/controllers/homescreen_controller.rb
+++ app/controllers/homescreen_controller.rb
@@ -2,7 +2,7 @@ class HomescreenController < ApplicationController
 	def index
 		@user_id = params[:id]
 		@orders = Order.where(user_id: @user_id).joins(:order_progress, model: :brand).select("orders.*, order_progresses.name, brands.name")
-		@last_order = @orders.last.id
+		@last_order = @orders.last.id unless @orders.last.nil?
 	end
 
 	def order
